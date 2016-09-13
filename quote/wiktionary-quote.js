c979739f2bf26dc475271d60866c68fc7af0c7e4
hugovk <hugovk@users.noreply.github.com>
2016-09-13 07:48:38 +0300

    Find term case-insensitive, but retain case in replacements

--- quote/wiktionary-quote.js
+++ quote/wiktionary-quote.js
@@ -26,6 +26,22 @@ function valueOfName(name) {
   return $("input[name=" + name + "]").val()
 }
 
+function preg_quote(str) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: booeyOH
+    // +   improved by: Ates Goral (http://magnetiq.com)
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   bugfixed by: Onno Marsman
+    // *     example 1: preg_quote("$40");
+    // *     returns 1: '\$40'
+    // *     example 2: preg_quote("*RRRING* Hello?");
+    // *     returns 2: '\*RRRING\* Hello\?'
+    // *     example 3: preg_quote("\\.+*?[^]$(){}=!<>|:");
+    // *     returns 3: '\\\.\+\*\?\[\^\]\$\(\)\{\}\=\!\<\>\|\:'
+
+    return (str+'').replace(/([\\\.\+\*\?\[\^\]\$\(\)\{\}\=\!\<\>\|\:])/g, "\\$1");
+}
+
 function buildQuote(type) {
 
   var text = "#* {{" + type;
@@ -55,7 +71,14 @@ function buildQuote(type) {
     if (input.attr('name') === "passage") {
       var term = $("input[name=term]").val().trim();
       if (term.length) {
-        var passage = input.val().trim().replace(term, "'''" + term + "'''");
+        var passage = input.val().trim();
+
+        // Add three single quotes each side for bold format
+        // Find case-insensitive, but retain case in replacements
+        passage = passage.replace(
+            new RegExp("(" + preg_quote(term) + ")", "ig"),
+             "'''$1'''");
+
         text += "<br>|";
         text += input.attr('name') + "=";
         text += passage;

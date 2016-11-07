ab0a7475ec6aa220736c7f8657217d5a6821a5d6
hugovk <hugovk@users.noreply.github.com>
2016-11-07 09:34:46 +0200

    Add clipboard copy button

--- quote/wiktionary-quote.js
+++ quote/wiktionary-quote.js
@@ -104,3 +104,10 @@ function buildQuote(type) {
   }
 }
 
+function copyToClipboard(element) {
+  var $temp = $('<input>');
+  $('body').append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand('copy');
+  $temp.remove();
+}
